3e8df26ba05598e0cb3de6d00ac3f770376582a3
Waltdev29 <waltdev29@gmail.com>
2025-09-22 19:39:42 +0900

    숫자이미지의 히스토그램 평균/분산을 구하여 이미지를 분류하는 코드 작성

--- 08_Draw.c
+++ 08_Draw.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <Windows.h>
-
+#include <math.h>
 
 
 HWND hwnd;	// handle window (윈도우 다루기)
@@ -27,6 +27,146 @@ typedef struct {		// 총 21 byte
 } ImageType;
 typedef ImageType* Image;
 
+void DrawImage(Image im);	// 숫자 이미지를 표시하는 함수
+void DrawHistogram(Image im, short* horizontal, short* vertical);	// 히스토그램을 표시하는 함수
+
+void calHistogram(Image im, short* horizontal, short* vertical);	// 히스토그램을 계산하는 함수
+void cal_variance_bias(Image im, float variance_arr[], float bias_arr[], short* horizontal, short* vertical);	// 히스토그램 평균/분산을 구하는 함수
+void classification(Image im, float minMaxRange[][8], char results[], float variance_arr[], float bias_arr[]);
+void equalize(Image im);	// 히스토그램 평활화를 적용하는 함수
+
+Image imageAllocate(unsigned int rows, unsigned int cols, char format, unsigned int levels);
+Image readPBMImage(const char* filename);
+void writePBMImage(const char* filename, const Image im);
+
+
+int main(void) {
+	Image img = 0;
+
+	float var_bias_arr[10][3][4];	// 각 숫자, 폰트별 평균과 분산을 저장하는 배열
+	float variance_arr[2];	// 평균을 저장하는 임시 배열
+	float bias_arr[2];	// 분산을 저장하는 임시 배열
+
+	// 각 숫자,폰트의 평균/분산을 구하는 부분
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 3; j++) {
+			char imgPath[] = "./pgm/no0-1.pgm";
+			imgPath[8] = '0' + i;
+			imgPath[10] = '1' + j;
+			img = readPBMImage(imgPath);
+			short* horizontal = malloc(sizeof(short) * img->cols);
+			short* vertical = malloc(sizeof(short) * img->rows);
+			calHistogram(img, horizontal, vertical);
+			cal_variance_bias(img, variance_arr, bias_arr, horizontal, vertical);
+			var_bias_arr[i][j][0] = variance_arr[0];
+			var_bias_arr[i][j][1] = variance_arr[1];
+			var_bias_arr[i][j][2] = bias_arr[0];
+			var_bias_arr[i][j][3] = bias_arr[1];
+			free(horizontal);
+			free(vertical);
+		}
+	}
+
+	//printf("1. var_row : %.2f\n", var_bias_arr[0][0][0]);
+	//printf("2. var_row : %.2f\n", var_bias_arr[0][1][0]);
+	//printf("3. var_row : %.2f\n\n", var_bias_arr[0][2][0]);
+
+	//printf("1. var_col : %.2f\n", var_bias_arr[0][0][1]);
+	//printf("2. var_col : %.2f\n", var_bias_arr[0][1][1]);
+	//printf("3. var_col : %.2f\n\n", var_bias_arr[0][2][1]);
+
+	//printf("1. bias_row : %.2f\n", var_bias_arr[0][0][2]);
+	//printf("2. bias_row : %.2f\n", var_bias_arr[0][1][2]);
+	//printf("3. bias_row : %.2f\n\n", var_bias_arr[0][2][2]);
+
+	//printf("1. bias_col : %.2f\n", var_bias_arr[0][0][3]);
+	//printf("2. bias_col : %.2f\n", var_bias_arr[0][1][3]);
+	//printf("3. bias_col : %.2f\n\n", var_bias_arr[0][2][3]);
+
+	// 숫자별 평균/분산의 범위를 구하는 부분
+	float minMaxRange[10][8];
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 4; j++) {
+			float v0 = var_bias_arr[i][0][j];
+			float v1 = var_bias_arr[i][1][j];
+			float v2 = var_bias_arr[i][2][j];
+
+			float mn = fminf(v0, fminf(v1, v2));
+			float mx = fmaxf(v0, fmaxf(v1, v2));
+
+			minMaxRange[i][j * 2] = mn;
+			minMaxRange[i][j * 2 + 1] = mx;
+		}
+		//printf("--- %d ---\n", i);
+		//printf("var_row_min : %.2f\n", minMaxRange[i][0]);
+		//printf("var_row_max : %.2f\n", minMaxRange[i][1]);
+		//printf("var_col_min : %.2f\n", minMaxRange[i][2]);
+		//printf("var_col_max : %.2f\n", minMaxRange[i][3]);
+		//printf("bias_row_min : %.2f\n", minMaxRange[i][4]);
+		//printf("bias_row_max : %.2f\n", minMaxRange[i][5]);
+		//printf("bias_col_min : %.2f\n", minMaxRange[i][6]);
+		//printf("bias_col_max : %.2f\n\n", minMaxRange[i][7]);
+	}
+
+	// 숫자 이미지를 읽어 분류하는 부분
+	while (1) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 3; j++) {
+				system("cls");
+				char imgPath[] = "./pgm/no0-1.pgm";
+				imgPath[8] = '0' + i;
+				imgPath[10] = '1' + j;
+				img = readPBMImage(imgPath);
+				DrawImage(img);
+				short* horizontal = (short*)malloc(sizeof(short) * img->cols);
+				short* vertical = (short*)malloc(sizeof(short) * img->rows);
+				char results[10] = { 0 };
+				calHistogram(img, horizontal, vertical);
+				cal_variance_bias(img, variance_arr, bias_arr, horizontal, vertical);
+				classification(img, minMaxRange, results, variance_arr, bias_arr);
+				printf("results : ");
+				for (int i = 0; i < 10; i++) {
+					if (results[i] >= 1) printf("%d ", i);
+				}
+				printf("\n");
+				free(horizontal);
+				free(vertical);
+
+				Sleep(1000);
+			}
+		}
+	}
+
+
+
+	/*while (1) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 3; j++) {
+				system("cls");
+				char imgPath[] = "./pgm/no0-1.pgm";
+				imgPath[8] = '0' + i;
+				imgPath[10] = '1' + j;
+				img = readPBMImage(imgPath);
+				DrawImage(img);
+
+				short* horizontal = malloc(sizeof(short) * img->cols);
+				short* vertical = malloc(sizeof(short) * img->rows);
+				calHistogram(img, horizontal, vertical);
+
+				printf("horizontal variance : %.2f\n", var_bias_arr[i][j][0]);
+				printf("vertical variance : %.2f\n", var_bias_arr[i][j][1]);
+				printf("horizontal bias : %.2f\n", var_bias_arr[i][j][2]);
+				printf("vertical bias : %.2f\n", var_bias_arr[i][j][3]);
+				DrawHistogram(img, horizontal, vertical);
+				free(horizontal);
+				free(vertical);
+				Sleep(1000);
+			}
+		}
+	}*/
+	return 0;
+}
+
 // 숫자 이미지를 표시하는 함수
 void DrawImage(Image im) {
 	hwnd = GetForegroundWindow();
@@ -42,42 +182,99 @@ void DrawImage(Image im) {
 	}
 }
 
-// 히스토그램을 표시하는 함수
-void DrawHistogram(Image im) {
-	// 가로/세로를 담을 배열 생성 후 초기화
-	short* horizontal = (short*)malloc(sizeof(short) * im->cols);
-	short* vertical = (short*)malloc(sizeof(short) * im->rows);
-
+// 히스토그램을 계산하는 함수
+void calHistogram(Image im, short* horizontal, short* vertical) {
+	// 배열 초기화
 	for (int i = 0; i < im->cols; i++) horizontal[i] = 0;
 	for (int i = 0; i < im->rows; i++) vertical[i] = 0;
 
-	// 이미지를 가로/세로로 순회하며 기준보다 어두운 색의 수를 기록
-	for (int i = 0; i < im->rows; ++i) {
-		for (int j = 0; j < im->cols; j++) {
-			if (im->content[i * im->cols + j] < 10) horizontal[j]++;
+	// 가로/세로를 순회하며 각 좌표별 픽셀 수를 기록
+	for (int y = 0; y < im->rows; ++y)
+		for (int x = 0; x < im->cols; x++) {
+			if (im->content[y * im->cols + x] < 10) horizontal[x]++;
+		}
+	for (int x = 0; x < im->cols; ++x) {
+		for (int y = 0; y < im->rows; y++) {
+			if (im->content[y * im->cols + x] < 10) vertical[y]++;
 		}
 	}
-	for (int j = 0; j < im->cols; ++j) {
-		for (int i = 0; i < im->rows; i++) {
-			if (im->content[i * im->cols + j] < 10) vertical[j]++;
-		}
-	}
+}
+
+// 히스토그램을 표시하는 함수
+void DrawHistogram(Image im, short* horizontal, short* vertical) {
 
 	hwnd = GetForegroundWindow();
 	hdc = GetWindowDC(hwnd);
 
-	// 가로/세로로 나아가며 히스토그램을 기록
+	// 가로/세로 히스토그램을 표시
 	for (int i = 0; i < im->cols; i++) {
-		for (int j = 0; j < horizontal[i] * 5; j++)
+		for (int j = 0; j < horizontal[i] * 5; j++) {
 			SetPixel(hdc, 250 + i, 250 - j, RGB(0, 255, 0));
+		}
 	}
 	for (int i = 0; i < im->rows; i++) {
-		for (int j = 0; j < vertical[i] * 5; j++)
+		for (int j = 0; j < vertical[i] * 5; j++) {
 			SetPixel(hdc, 250 - j, 250 + i, RGB(0, 255, 0));
+		}
 	}
 
 	free(horizontal);
 	free(vertical);
+}
+
+// 히스토그램 평균/분산을 구하는 함수
+void cal_variance_bias(Image im, float variance_arr[], float bias_arr[], short* horizontal, short* vertical) {
+	// 각 (좌표*픽셀)들의 총합
+	short horizontal_sum = 0;
+	short vertical_sum = 0;
+
+	// 가로/세로별 총 픽셀 개수
+	short horizontal_count = 0;
+	short vertical_count = 0;
+
+	// 총 픽셀 개수와 (좌표*픽셀)들의 총합을 구하는 반복문
+	for (int i = 0; i < im->cols; i++) {
+		horizontal_count += horizontal[i];
+		horizontal_sum += (i + 1) * horizontal[i];
+	}
+	for (int i = 0; i < im->rows; i++) {
+		vertical_count += vertical[i];
+		vertical_sum += (i + 1) * vertical[i];
+	}
+
+	// 평균 구하기
+	float horizontal_variance = horizontal_sum / (float)horizontal_count;
+	float vertical_variance = vertical_sum / (float)vertical_count;
+
+	// 평균 반환
+	variance_arr[0] = horizontal_variance;
+	variance_arr[1] = vertical_variance;
+
+	short horizontal_bias_sum = 0;
+	short vertical_bias_sum = 0;
+
+	// 픽셀 개수 * (좌표 - 평균)을 구하는 반복문
+	for (int i = 0; i < im->cols; i++) {
+		horizontal_bias_sum += horizontal[i] * pow((i + 1) - horizontal_variance, 2);
+	}
+	for (int i = 0; i < im->rows; i++) {
+		vertical_bias_sum += vertical[i] * pow((i + 1) - vertical_variance, 2);
+	}
+
+	// 분산 반환
+	bias_arr[0] = horizontal_bias_sum / (float)horizontal_count;
+	bias_arr[1] = vertical_bias_sum / (float)vertical_count;
+}
+
+void classification(Image im, float minMaxRange[][8], char results[], float variance_arr[], float bias_arr[]) {
+	int num;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 2; j++) {
+			if ((minMaxRange[i][0] <= variance_arr[0] && variance_arr[0] <= minMaxRange[i][1]) && (minMaxRange[i][2] <= variance_arr[1] && variance_arr[1] <= minMaxRange[i][3]))
+				if ((minMaxRange[i][4] <= bias_arr[0] && bias_arr[0] <= minMaxRange[i][5]) && (minMaxRange[i][6] <= bias_arr[1] && bias_arr[1] <= minMaxRange[i][7]))
+					results[i] = 1;
+		}
+	}
 }
 
 // 히스토그램 평활화를 적용하는 함수
@@ -224,24 +421,4 @@ void writePBMImage(const char* filename, const Image im) {
 	}
 
 	fclose(pgmFile);
-}
-
-int main(void) {
-	Image img = 0;
-
-	while (1) {
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 3; j++) {
-				system("cls");
-				char imgPath[] = "./pgm/no0-1.pgm";
-				imgPath[8] = '0' + i;
-				imgPath[10] = '1' + j;
-				img = readPBMImage(imgPath);
-				DrawImage(img);
-				DrawHistogram(img);
-				Sleep(1000);
-			}
-		}
-	}
-	return 0;
 }
