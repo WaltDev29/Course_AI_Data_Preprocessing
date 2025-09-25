a4080e8698acd574eb8b4fc8d38c352d0d85ce62
Waltdev29 <waltdev29@gmail.com>
2025-09-25 20:05:52 +0900

    함수로 분리함

--- 12_csv_assign.c
+++ 12_csv_assign.c
@@ -1,58 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+#define FILE_NAME "simple.csv"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-int main(void) {
-	char str_tmp[1024];	// 문자열 저장할 임시 버퍼
-	int** scores;	// 점수들 넣을 배열	
-	char* token = NULL;	// strtok 반환값 저장할 임시 버퍼
-	int iSize = 0;	// 배열 행 개수
-	int* jSize;	// 배열 행별 열 개수
+// 배열 행/열 사이즈 구하는 함수
+int* getSizes(FILE* fp, int* iSize) {	
+	char str_tmp[1024];
+	char* token;
+	int* jSize = (int*)malloc(sizeof(int) * (*iSize));
 
-	// 파일 열기
-	FILE* fp = fopen("simple.csv", "rt");
-	if (fp == NULL) {
-		printf("파일을 읽어오지 못했습니다.\n");
-		return -1;
-	}
-
-
-	// 배열 행/열 사이즈 구하기
-	jSize = (int*)malloc(sizeof(int) * iSize);
 	while (fgets(str_tmp, sizeof(str_tmp), fp) != NULL) {
-		iSize++;
-		jSize = (int*)realloc(jSize, sizeof(int) * iSize);
-		jSize[iSize - 1] = 0;
+		(*iSize)++;
+		jSize = (int*)realloc(jSize, sizeof(int) * *iSize);
+		jSize[*iSize - 1] = 0;
 		token = strtok(str_tmp, ",");
 		while (token != NULL) {
-			jSize[iSize - 1]++;
+			jSize[*iSize - 1]++;
 			token = strtok(NULL, ",");
 		}
 	}
+	return jSize;
+}
 
+// 배열 동적 할당 및 파일 내용 대입하는 함수
+int** allotArray(FILE* fp, int iSize, int* jSize) {
+	char str_tmp[1024];
+	char* token = NULL;
 
-	// 배열 행/열 사이즈 출력
-	printf("iSize : %d\n", iSize);
-	for (int i = 0; i < 2; i++) {
-		printf("jSize[%d] : %d\n", i, jSize[i]);
-	}
-	printf("\n");
-
-	// 배열 동적 할당
-	scores = (int**)malloc(sizeof(int*) * iSize);
+	// 배열 동적 생성
+	int** scores = (int**)malloc(sizeof(int*) * iSize);	
 	for (int i = 0; i < iSize; i++) {
 		scores[i] = (int*)malloc(sizeof(int) * jSize[i]);
 	}
 
-	// 파일 읽어서 배열에 할당
+	// 배열에 파일 내용 대입
 	fseek(fp, SEEK_SET, 0);
-
 	for (int i = 0; i < iSize; i++) {
 		fgets(str_tmp, sizeof(str_tmp), fp);
+
 		token = strtok(str_tmp, ",");
 		int num = atoi(token);
 		scores[i][0] = num;
+
 		for (int j = 1; j < jSize[i]; j++) {
 			token = strtok(NULL, ",");
 			num = atoi(token);
@@ -60,13 +52,43 @@ int main(void) {
 		}
 	}
 
-	// 점수 출력
-	for (int i = 0; i < iSize; i++) {
-		for (int j = 0; j < jSize[i]; j++) {
-			printf("scores[%d][%d] = %d\n", i, j, scores[i][j]);
-		}
-		printf("\n");
+	return scores;
+}
+
+int main(void) {	
+	int** scores;	// 점수들 넣을 배열		
+	int iSize = 0;	// 배열 행 개수
+	int* jSize;	// 배열 행별 열 개수
+
+	// 파일 열기
+	FILE* fp = fopen(FILE_NAME, "rt");
+	if (fp == NULL) {
+		printf("파일을 읽어오지 못했습니다.\n");
+		return -1;
 	}
+
+	// 배열 행/열 사이즈 구하기
+	jSize = getSizes(fp, &iSize);
+
+	// 배열 행/열 사이즈 출력
+	//printf("iSize : %d\n", iSize);
+	//for (int i = 0; i < 2; i++) {
+	//	printf("jSize[%d] : %d\n", i, jSize[i]);
+	//}
+	//printf("\n");
+
+	// 배열 동적 할당 및 파일 내용 대입
+	scores = allotArray(fp, iSize, jSize);
+
+	// 배열 출력
+	//for (int i = 0; i < iSize; i++) {
+	//	for (int j = 0; j < jSize[i]; j++) {
+	//		printf("scores[%d][%d] = %d\n", i, j, scores[i][j]);
+	//	}
+	//	printf("\n");
+	//}
+
+
 
 	fclose(fp);
 	free(jSize);
@@ -74,9 +96,6 @@ int main(void) {
 	return 0;
 }
 
-// 1. fgets로 읽어올 때마다 인덱스 증가 (몇 행인지 확인) 
-// 2. seek처음으로 돌리고 malloc으로 배열 만들기
-// 3. str_tmp 복사해서 str_cpy만들고 이거 token만들 때마다 인덱스 증가 (컬럼 수 확인)
-// 4. 각 행별로 malloc으로 배열 만들어서 삽입
-// 5. 각 행별로 str_tmp strtok해서 배열에 넣기
-// DONE.
+// to-do
+// 1. 애들 평균 계산하기
+// 2. 과목별 평균 계산하기
